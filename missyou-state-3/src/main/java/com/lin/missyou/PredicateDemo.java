33188f31ee6f7e8957d532683cd75fde4c1693fe
cassie <cassie@cassiedeMacBook-Pro.local>
2023-01-23 15:03:19 +0800

    init project

--- missyou-state-3/src/main/java/com/lin/missyou/PredicateDemo.java
+++ missyou-state-3/src/main/java/com/lin/missyou/PredicateDemo.java
@@ -1,4 +1,12 @@
 package com.lin.missyou;
 
+import java.io.File;
+import java.util.function.Predicate;
+
 public class PredicateDemo {
+
+    public static void main(String[] args) {
+        Predicate<File> predicate  = file -> true;
+    }
+
 }
